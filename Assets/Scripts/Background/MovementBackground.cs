a1543bf376a02264a42f996c51c8612e9434b7cf
Adriaaan63 <adcast07@ucm.es>
2023-02-21 18:56:26 +0100

    movimiento fondo

---
+++ Assets/Scripts/Background/MovementBackground.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovementBackground : MonoBehaviour
+{
+    [SerializeField] private Vector2 _velocity;
+
+    private Vector2 _offset;
+
+    private Material _material;
+
+    [SerializeField] private Rigidbody2D _rbplayer;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        _material = GetComponent<SpriteRenderer>().material;
+        _rbplayer = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        _offset = (_rbplayer.velocity.x * 0.1f) * _velocity* Time.deltaTime;
+        _material.mainTextureOffset += _offset;
+    }
+}
