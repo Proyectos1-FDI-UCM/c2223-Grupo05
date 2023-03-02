f1f41a1bd01b28f82e76c249c44ce2ce6cadb86c
Marcos Perez Martinez <120493533+Marc0sPM@users.noreply.github.com>
2023-03-02 01:11:43 +0100

    Cositas de plumas NO AGUANTO MAS ;P

--- Assets/Scripts/Feather/FeatherStates.cs
+++ Assets/Scripts/Feather/FeatherStates.cs
@@ -5,10 +5,10 @@ using UnityEngine;
 
 public class FeatherStates : MonoBehaviour
 {
-    public enum FeatherState {FEATHER, PLATFORM, RETURN}
+    public enum FeatherState { FEATHER, PLATFORM, RETURN }
 
     private FeatherState _currentState;
-   
+
     private FeatherState _nextState;
 
     private void EnterState(FeatherState state)
@@ -17,27 +17,20 @@ public class FeatherStates : MonoBehaviour
         {
             case FeatherState.FEATHER:
                 this.gameObject.transform.GetChild((int)FeatherState.FEATHER).gameObject.SetActive(true);
-                
+                GetComponent<Rigidbody2D>().bodyType = RigidbodyType2D.Dynamic;
+                GetComponent<FeatherComponent>().enabled = true;
+                GetComponent<PolygonCollider2D>().enabled = true;
+
                 break;
             case FeatherState.PLATFORM:
                 this.gameObject.transform.GetChild((int)FeatherState.PLATFORM).gameObject.SetActive(true);
-<<<<<<< Updated upstream
-                GetComponent<Rigidbody2D>().bodyType=  RigidbodyType2D.Static;
+                GetComponent<Rigidbody2D>().bodyType = RigidbodyType2D.Static;
                 GetComponent<FeatherComponent>().enabled = false;
-                GetComponent<PolygonCollider2D>().enabled= false;
-                transform.rotation = Quaternion.identity;             
+                GetComponent<PolygonCollider2D>().enabled = false;
+                transform.rotation = Quaternion.identity;
 
                 break;
             case FeatherState.RETURN:
-=======
-                GetComponent<Rigidbody2D>().bodyType = RigidbodyType2D.Static;
-                transform.rotation = Quaternion.identity;
-                break;
-            case FeatherState.RETURN:
-                this.gameObject.transform.GetChild((int)FeatherState.RETURN).gameObject.SetActive(true);
-                GetComponent<Rigidbody2D>().bodyType = RigidbodyType2D.Dynamic;
-                this.gameObject.transform.GetChild((int)FeatherState.RETURN).gameObject.GetComponent<FeatherReturn>().ActivateReturn();
->>>>>>> Stashed changes
 
                 break;
         }
@@ -51,13 +44,8 @@ public class FeatherStates : MonoBehaviour
 
                 break;
             case FeatherState.PLATFORM:
-                this.gameObject.transform.GetChild((int)FeatherState.PLATFORM ).gameObject.SetActive(false);
-<<<<<<< Updated upstream
-            
-=======
-                break;
-            case FeatherState.RETURN:
->>>>>>> Stashed changes
+                this.gameObject.transform.GetChild((int)FeatherState.PLATFORM).gameObject.SetActive(false);
+
                 break;
         }
     }
@@ -66,30 +54,21 @@ public class FeatherStates : MonoBehaviour
     {
         _nextState = newState;
     }
-    private void Awake()
-    {
-        this.gameObject.transform.GetChild((int)FeatherState.FEATHER).gameObject.SetActive(false);
-        this.gameObject.transform.GetChild((int)FeatherState.PLATFORM).gameObject.SetActive(false);
-        this.gameObject.transform.GetChild((int)FeatherState.RETURN).gameObject.SetActive(false);
-    }
     // Start is called before the first frame update
     void Start()
     {
-        
         _currentState = FeatherState.PLATFORM;
         _nextState = FeatherState.FEATHER;
-        
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(_nextState != _currentState)
+        if (_nextState != _currentState)
         {
             ExitState(_currentState);
             _currentState = _nextState;
             EnterState(_currentState);
         }
-        Debug.Log(_currentState);
     }
 }
