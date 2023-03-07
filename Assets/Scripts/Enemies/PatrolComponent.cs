7f0f4b3b885b3d67fa4efaffde73b934efbfa4f2
Mikeey84 <milope21@ucm.es>
2023-03-07 19:09:01 +0100

    Enemigos patrol

---
+++ Assets/Scripts/Enemies/PatrolComponent.cs
@@ -0,0 +1,67 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PatrolComponent : MonoBehaviour
+{
+    Rigidbody2D _enemyRB;
+    Transform _enemyTransform;
+    private bool _lookingRight;
+    private bool _touchingFloor;
+
+    [Header("Move")]
+
+    [SerializeField] private LayerMask _floor;
+    [SerializeField] private float _moveSpeed;
+    [SerializeField] private Transform _detectorControler;
+    [SerializeField] private Vector2 _detectorDimensions;
+    [SerializeField] private float _timeCoolDown;
+
+    private float _time;
+
+    private float _direction = 1;
+
+    RaycastHit2D _floorRay;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _enemyRB = GetComponent<Rigidbody2D>();
+        _enemyTransform = transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+        if(!_touchingFloor)
+        {
+            Debug.Log("Para");
+            _enemyRB.velocity = new Vector2(0, 0);
+            if(_time > _timeCoolDown)
+            {
+                Turn(ref _moveSpeed);
+                Move();
+                _time = 0;
+            }
+            _time += Time.deltaTime;
+        }
+    }
+    private void FixedUpdate()
+    {
+        _touchingFloor = Physics2D.OverlapBox(_detectorControler.position, _detectorDimensions, 0, _floor);
+    }
+    private void Move()
+    {
+        _enemyRB.velocity = new Vector2(_moveSpeed * _direction * -1, _enemyRB.velocity.y);
+    }
+
+    private void Turn(ref float _direction)
+    {
+        _lookingRight = !_lookingRight;
+        Vector3 scale = transform.localScale;
+        scale.x *= -1;
+        transform.localScale = scale;
+        _direction *= -1;
+    }
+}
