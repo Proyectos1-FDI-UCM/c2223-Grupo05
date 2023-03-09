0b8b9bbbcad8beb03866ec55d9387d821b3f1f88
Mikeey84 <milope21@ucm.es>
2023-03-09 21:26:23 +0100

    Animacion muerte mele

--- Assets/Scripts/Enemies/LifeEnemyComponent.cs
+++ Assets/Scripts/Enemies/LifeEnemyComponent.cs
@@ -7,22 +7,31 @@ public class LifeEnemyComponent : MonoBehaviour
     [SerializeField]
     private int _maxHealth;
     int _currentHealth;
+    private bool _isDeath = false;
+    private Animator _animator;
     // Start is called before the first frame update
     void Start()
     {
         _currentHealth = _maxHealth;
+        _animator = GetComponent<Animator>();
     }
-
+    void Update()
+    {
+        _animator.SetBool("Death", _isDeath);
+        if (_currentHealth < 0)
+        {
+            this.gameObject.GetComponent<Rigidbody2D>().velocity = Vector2.zero;
+            this.gameObject.GetComponent<PatrolComponent>().enabled = false;
+            this.gameObject.GetComponent<SpinComponent>().enabled = false;
+            this.gameObject.GetComponent<CircleCollider2D>().enabled = false;
+            _isDeath = true;
+        }
+    }
     public void TakeDamage(int damage)
     {
         _currentHealth -= damage;
-
-        if (_currentHealth <= 0)
-        {
-            Die();
-        }
     }
-    private void Die()
+    public void Die()
     {
         //animación de muerte
         Destroy(gameObject);
