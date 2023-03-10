a79258ad93598bd1acfd5c6166bd81fdd95a9096
Marcos Perez Martinez <120493533+Marc0sPM@users.noreply.github.com>
2023-03-10 01:27:15 +0100

    Animacion boton

--- Assets/Scripts/Button/ButtonComponent.cs
+++ Assets/Scripts/Button/ButtonComponent.cs
@@ -6,6 +6,7 @@ using UnityEngine.Rendering.Universal;
 public class ButtonComponent : MonoBehaviour
 {
     [SerializeField] private PlatformComponent _myPlatformComponent;
+    private Animator _animator;
 
     private bool _buttonActivated = false;
 
@@ -26,14 +27,14 @@ public class ButtonComponent : MonoBehaviour
             collision.gameObject.GetComponentInParent<FeatherStates>().ChangeState(FeatherStates.FeatherState.RETURN);
         }
     }
-
-    //private void Update()
-    //{
-    //    if (_buttonActivated && _myPlatformComponent.MoveTowardsTarget == false && _myPlatformComponent.gameObject.transform.position == _myPlatformComponent.TargetPosition.position)
-    //    {
-    //        _myPlatformComponent.ChangeMove();
-    //    }
-    //}
+    private void Start()
+    {
+        _animator = GetComponent<Animator>();
+    }
+    private void Update()
+    {
+        _animator.SetBool("isActivated", _buttonActivated);
+    }
 
     private void OnTriggerExit2D(Collider2D collision)
     {
