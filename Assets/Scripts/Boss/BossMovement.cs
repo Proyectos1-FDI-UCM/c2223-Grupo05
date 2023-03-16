e82c28ca5e04cc8d9278cc156d631af45abc1e74
Vivu24 <93614868+Vivu24@users.noreply.github.com>
2023-03-16 12:50:32 +0100

    Un poco más de boss

--- Assets/Scripts/Boss/BossMovement.cs
+++ Assets/Scripts/Boss/BossMovement.cs
@@ -5,11 +5,22 @@ using UnityEngine;
 public class BossMovement : MonoBehaviour
 {
     [SerializeField] private BossManager _myBossManager;
+    private Rigidbody2D _myrigidbody2D;
 
+    [SerializeField] private float _movingTime;
+
+
+    #region Methods
+    private IEnumerator SimpleMovement()
+    {
+        
+        yield return new WaitForSeconds(_movingTime);
+    }
+    #endregion
     // Start is called before the first frame update
     void Start()
     {
-        
+        _myrigidbody2D = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
