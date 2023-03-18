6b6f0c095eebf49122fe6109de2cb5f684cca313
[MarcosPRC] <[marcopan@ucm]>
2023-03-18 10:41:12 +0100

    UI Manager

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -1,9 +1,35 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class UIManager : MonoBehaviour
 {
+    #region references
+    [SerializeField] private TMP_Text _move;
+    [SerializeField] private TMP_Text _jump;
+    [SerializeField] private TMP_Text _interact;
+    [SerializeField] private TMP_Text _feather;
+    [SerializeField] private TMP_Text _dash;
+    [SerializeField] private TMP_Text _sword;
+    [SerializeField] private TMP_Text _button;
+
+    [SerializeField] private 
+    
+
+    //MENUS
+    [SerializeField] private GameObject _startMenu;
+    [SerializeField] private GameObject _gameplayHUD;
+    [SerializeField] private GameObject _retryMenu;
+    [SerializeField] private GameObject _gameOverMenu;
+    #endregion
+
+    #region properties
+    //array de distintos menus
+    private GameObject[] _menus;
+    #endregion
+    #region methods
+    #endregion
     // Start is called before the first frame update
     void Start()
     {
