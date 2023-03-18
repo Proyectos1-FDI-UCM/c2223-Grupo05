cdb333c22f8b01ba37a5ef8c0bbed959d60970ea
Vivu24 <93614868+Vivu24@users.noreply.github.com>
2023-03-18 18:09:04 +0100

    Comienzo ataque boss

--- Assets/Scripts/Boss/Boss_Movement.cs
+++ Assets/Scripts/Boss/Boss_Movement.cs
@@ -7,7 +7,8 @@ public class Boss_Movement : StateMachineBehaviour
 
     [SerializeField] private float _speed;
     private bool _turned;
-    Vector3 flipped;
+    Vector3 _flipped;
+    private float _attackDistance = 5f;
 
     private Transform _player;
     //private Rigidbody2D _myRigidBody2D;
@@ -17,32 +18,33 @@ public class Boss_Movement : StateMachineBehaviour
     // OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
     override public void OnStateEnter(Animator animator, AnimatorStateInfo stateinfo, int layerindex)
     {
-        Debug.Log("Entro al estado");
         _player = GameManager.Instance.SetPlayer().transform;
-        //_myRigidBody2D = animator.GetComponent<Rigidbody2D>();
-        flipped = animator.transform.localScale;
+        _flipped = animator.transform.localScale;
     }
 
     // OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
     override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
-        Debug.Log("Dentro del Update");
         _playerPosition = new Vector2(_player.position.x, animator.transform.position.y);
         Debug.Log(_playerPosition);
         animator.transform.position = Vector2.MoveTowards(animator.transform.position, _playerPosition, _speed * Time.fixedDeltaTime);
         LookingPlayer(animator);
-        //_myRigidBody2D.MovePosition(_playerPosition);
+
+        if (Vector2.Distance(_player.transform.position, animator.transform.position) <= _attackDistance)
+        {
+            animator.SetTrigger("Attack");
+        }
     }
 
     // OnStateExit is called when a transition ends and the state machine finishes evaluating this state
     override public void OnStateExit(Animator animator, AnimatorStateInfo stateinfo, int layerindex)
     {
-
+        animator.ResetTrigger("Attack");
     }
 
     private void LookingPlayer(Animator animator)
     {
-        flipped.z *= -1;
+        _flipped.z *= -1;
 
         if (animator.transform.position.x > _player.position.x && _turned)
         {
@@ -57,7 +59,7 @@ public class Boss_Movement : StateMachineBehaviour
 
     private void Turn(Animator animator)
     {
-        animator.transform.localScale = flipped;
+        animator.transform.localScale = _flipped;
         animator.transform.Rotate(0, 180, 0);
         _turned = !_turned;
     }
