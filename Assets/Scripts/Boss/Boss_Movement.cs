bf789899eeab1d0a3f1b926174a8702d7476b2f3
Vivu24 <93614868+Vivu24@users.noreply.github.com>
2023-03-18 17:31:34 +0100

    El boss se mueve (no rigidbody)

--- Assets/Scripts/Boss/Boss_Movement.cs
+++ Assets/Scripts/Boss/Boss_Movement.cs
@@ -7,7 +7,7 @@ public class Boss_Movement : StateMachineBehaviour
     [SerializeField] private float _speed;
 
     private Transform _player;
-    private Rigidbody2D _myRigidBody2D;
+    //private Rigidbody2D _myRigidBody2D;
 
     private Vector2 _playerPosition;
 
@@ -15,16 +15,18 @@ public class Boss_Movement : StateMachineBehaviour
     override public void OnStateEnter(Animator animator, AnimatorStateInfo stateinfo, int layerindex)
     {
         Debug.Log("Entro al estado");
-        _player = GameObject.FindGameObjectWithTag("Player").transform;
-        _myRigidBody2D = animator.GetComponent<Rigidbody2D>();
+        _player = GameManager.Instance.SetPlayer().transform;
+        //_myRigidBody2D = animator.GetComponent<Rigidbody2D>();
     }
 
     // OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
     override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
         Debug.Log("Dentro del Update");
-        _playerPosition = new Vector2(_player.position.x, _myRigidBody2D.position.y);
-        Vector2.MoveTowards(_myRigidBody2D.position, _playerPosition, _speed * Time.fixedDeltaTime);
+        _playerPosition = new Vector2(_player.position.x, animator.gameObject.transform.position.y);
+        Debug.Log(_playerPosition);
+        animator.gameObject.transform.position = Vector2.MoveTowards(animator.gameObject.transform.position, _playerPosition, _speed * Time.fixedDeltaTime);
+        //_myRigidBody2D.MovePosition(_playerPosition);
     }
 
     // OnStateExit is called when a transition ends and the state machine finishes evaluating this state
