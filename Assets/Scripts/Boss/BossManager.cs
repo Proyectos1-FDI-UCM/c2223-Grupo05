a9811cd02dc105d8a71962bfa8a49ac675b79f8a
Vivu24 <93614868+Vivu24@users.noreply.github.com>
2023-03-19 16:44:12 +0100

    Bastante avance Boss

--- Assets/Scripts/Boss/BossManager.cs
+++ Assets/Scripts/Boss/BossManager.cs
@@ -8,25 +8,30 @@ public class BossManager : MonoBehaviour
     [SerializeField] private int _teleportCounter;
     [SerializeField] private int _teleportsDesired;
 
-    [SerializeField] private float _CurrentHitsReceived;       //Quitar SerializeField
+    [SerializeField] private int _enemiesCounter;
+    [SerializeField] private int _enemiesDesired;
+
+    [SerializeField] private float _currentHitsReceived;       //Quitar SerializeField
     [SerializeField] private float _maxHitsReceivedForTeleport;
+    [SerializeField] private float _maxHitsReceivedForEnemy;
+    [SerializeField] private float _auxHitsReceived;
 
     private Animator _myAnimator;
 
     #region Methods
     private void ReceiveDamage(int damageReceived)
     {
-        _CurrentHitsReceived += damageReceived;
+        _currentHitsReceived += damageReceived;
     }
     private void CheckAction()      // Cada vez que recibamos daño, verá que acción deberá hacer el boss
     {
-        if (_CurrentHitsReceived == _maxHitsReceivedForTeleport && _teleportCounter < _teleportsDesired)
+        if (_currentHitsReceived == _maxHitsReceivedForTeleport && _teleportCounter < _teleportsDesired)
         {
             _teleportCounter++;
             _myAnimator.SetTrigger("Teleport");
             if (_teleportCounter != _teleportsDesired)
             {
-                _CurrentHitsReceived = 0;
+                _currentHitsReceived = 0;
             }
         }
     }
@@ -40,13 +45,35 @@ public class BossManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (_CurrentHitsReceived == _maxHitsReceivedForTeleport && _teleportCounter <T _teleportsDesired)          // Quitar de update, está para probar
+        if (_currentHitsReceived == _maxHitsReceivedForTeleport && _teleportCounter < _teleportsDesired)          // Quitar de update, está para probar
         {
             _teleportCounter++;
             _myAnimator.SetTrigger("Teleport");
             if (_teleportCounter != _teleportsDesired)
             {
-                _CurrentHitsReceived = 0;
+                _currentHitsReceived = 0;
+            }
+            else
+            {
+                _auxHitsReceived = _currentHitsReceived;
+                _myAnimator.SetBool("isPhase1", false);
+                _myAnimator.SetBool("isPhase2", true);
+            }
+        }
+
+        if (_currentHitsReceived == _maxHitsReceivedForEnemy && _enemiesCounter < _enemiesDesired)              // Qujitar de update
+        {
+            _enemiesCounter++;
+            _myAnimator.SetTrigger("Throw Enemy");
+            if (_enemiesCounter != _enemiesDesired)
+            {
+                _currentHitsReceived = _auxHitsReceived;
+            }
+            else
+            {
+                _auxHitsReceived = _currentHitsReceived;
+                _myAnimator.SetBool("isPhase2", false);
+                _myAnimator.SetBool("isPhase3", true);
             }
         }
     }
--- Assets/Scripts/Boss/BossThrowEnemy.cs
+++ Assets/Scripts/Boss/BossThrowEnemy.cs
@@ -5,8 +5,12 @@ using UnityEngine;
 public class BossThrowEnemy : MonoBehaviour
 {
     [SerializeField] private GameObject _myEnemyPrefab;
-
+    [SerializeField] Transform _enemySpawnPoint;
     // Start is called before the first frame update
+    public void SpawnEnemy()
+    {
+        Instantiate(_myEnemyPrefab, _enemySpawnPoint.position, Quaternion.identity);
+    }
     void Start()
     {
         
