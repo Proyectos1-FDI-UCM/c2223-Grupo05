22cb3b07cb21c1c97c1a775a61425f7af0786727
Marcos Perez Martinez <120493533+Marc0sPM@users.noreply.github.com>
2023-03-20 21:33:57 +0100

    Haciendo arreglitos al knockback
    NO TOCAR

--- Assets/Scripts/RecoilComponent.cs
+++ Assets/Scripts/RecoilComponent.cs
@@ -7,10 +7,7 @@ public class RecoilComponent : MonoBehaviour
 {
     private Rigidbody2D _rb;
     [SerializeField] private float _recForceX;
-    [SerializeField] private float _recForceY;
-    // Start is called before the first frame update
-    private float _counter = 0;
-    [SerializeField] private float _waitTime;
+    
     void Start()
     {
         _rb = GetComponent<Rigidbody2D>();
@@ -22,19 +19,7 @@ public class RecoilComponent : MonoBehaviour
         Vector3 aux = new Vector3(direction, 0, 0).normalized; //direction into 1 / -1
         _rb.AddForce(new Vector3( _recForceX * aux.x,0, 0), ForceMode2D.Impulse);
 
-        while(_counter < _waitTime)
-        {
-            _counter += Time.deltaTime;
-        }
-        if (_counter >= _waitTime)
-        {
-            if ((bool)this.gameObject.GetComponent<SpinComponent>())
-            {
-                GetComponent<PatrolComponent>().enabled = true;
-            }
-            
-            _counter = 0;
-        }
+        
 
 
     }
