3ba780e5656635497d68b24822a553647b35cc83
Marcos Perez Martinez <120493533+Marc0sPM@users.noreply.github.com>
2023-03-22 20:16:50 +0100

    Knockback terminadisimo y modificaiones en enemy mele

--- Assets/Scripts/RecoilComponent.cs
+++ Assets/Scripts/RecoilComponent.cs
@@ -7,6 +7,8 @@ public class RecoilComponent : MonoBehaviour
     private Rigidbody2D _rb2D;
     [SerializeField] private float _recForce;
     [SerializeField] private float _delay;
+
+
     
     void Start()
     {
@@ -15,16 +17,48 @@ public class RecoilComponent : MonoBehaviour
     public void KnockBack(GameObject sender)
     {
         StopAllCoroutines();
-        Vector2 direction = (transform.position - sender.transform.position).normalized;
+        float individualDirection = 1;
+        if ((bool)this.GetComponent<InputComponent>())
+        {
+            individualDirection *= -1;
+        }
+        Vector2 direction;
+        if (sender.transform.localScale.x > 0)
+        {
+            direction = new Vector2(individualDirection * _recForce, 3);
+        }
+        else
+        {
+            direction = new Vector2(-individualDirection * _recForce, 3);
+        }
+        EnabledDisabled(false); // Disable all needed components
+        
         
         _rb2D.AddForce(direction * _recForce, ForceMode2D.Impulse);
         StartCoroutine(Reset());
+    }
+    private void EnabledDisabled(bool aux)
+    {
+        if ((bool)GetComponent<InputComponent>())
+        {
+            this.GetComponent<InputComponent>().enabled = aux;
+            this.GetComponent<MovementComponent>().enabled = aux;
+        }
+        else
+        {
+            if ((bool)GetComponent<SpinComponent>())
+            {
+                this.GetComponent<SpinComponent>().enabled = aux;
+            }
+            this.GetComponent<PatrolComponent>().enabled = aux;
+        }
     }
 
 
     public IEnumerator Reset()
     {
         yield return new WaitForSeconds(_delay);
+        EnabledDisabled(true); //Enabled all needed components
         _rb2D.velocity = Vector3.zero;
     }
 }
