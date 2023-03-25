ba4fa589c2620d2907742abc19e3235b30c2dd9b
Mikeey84 <milope21@ucm.es>
2023-03-26 00:05:43 +0100

    Puerta para pasar de nivel

---
+++ Assets/Scripts/Doors/DoorComponent.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class DoorComponent : MonoBehaviour
+{
+    [Header("INDEX")]
+    [SerializeField]
+    private int _myIndex;
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (collision.gameObject.GetComponent<InputComponent>() && Input.GetKeyDown(KeyCode.E))
+        {
+            CambiarNivel(_myIndex);
+        }
+    }
+    public void CambiarNivel(int _index)
+    {
+        SceneManager.LoadScene(_index);
+    }
+}
