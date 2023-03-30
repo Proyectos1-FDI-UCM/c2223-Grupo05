ba568951d45bc54f66b91710c14d91170c5fa865
Adriaan63 <Adcast07@ucm.es>
2023-03-30 15:02:48 +0200

    luego se sigue e respawn

--- Assets/Scripts/Player/RespawnComponent.cs
+++ Assets/Scripts/Player/RespawnComponent.cs
@@ -5,12 +5,14 @@ public class RespawnComponent : MonoBehaviour
     private float _gravIni;
     private Rigidbody2D _rb;
     private bool _isDeath = false;
+    private bool _isRespawn = false;
     
     
     void Start()
     {
         _gravIni = GetComponent<Rigidbody2D>().gravityScale;
         _rb = GetComponent<Rigidbody2D>();
+        
     }
     private void Update()
     {
@@ -18,40 +20,70 @@ public class RespawnComponent : MonoBehaviour
         {
             Die();
         }
+        if(GetComponent<MovementComponent>().TouchingFloor && _isRespawn)
+        {
+            Respawn();
+        }
     }
 
     public void Die() //Desactiva componentes de player que interactuan con el mapa
     {
-        GetComponent<CapsuleCollider2D>().enabled = false;
+
         GetComponent<InputComponent>().enabled = false;
-        _rb.gravityScale = 0;
+        DisablePhysics();
+        _rb.gravityScale = 6f;
         _rb.velocity = Vector2.zero;
-        for (int i = 0; i < transform.childCount; i++) //Desactiva componentes hijos
+        if(GetComponent<MovementComponent>().TouchingFloor)
+        {
+            _isRespawn = true;
+        }
+
+        /*for (int i = 0; i < transform.childCount; i++) //Desactiva componentes hijos
         {
             transform.GetChild(i).gameObject.SetActive(value: false);
+        }*/
+    }
+    private void DisablePhysics()
+    {
+        Collider2D[] colliders = GetComponents<Collider2D>();
+        foreach (Collider2D collider in colliders)
+        {
+            collider.enabled = false;
+        }
+    }
+    private void EnablePhysics()
+    {
+        Collider2D[] colliders = GetComponents<Collider2D>();
+        foreach (Collider2D collider in colliders)
+        {
+            collider.enabled = true;
         }
     }
     public void ActivateDeath()
     {
         _isDeath = true;
     }
+    /*public void AtivateRespawn()
+    {
+        _isRespawn = true;
+    }*/
 
     public void Respawn() //Llamar cuando tengamos HUD y esas vainas
     {
         SoundComponent.Instance.PlaySound(SoundComponent.Instance._respawn);
-        for (int i = 0; i < transform.childCount; i++)
+        /*for (int i = 0; i < transform.childCount; i++)
         {
             transform.GetChild(i).gameObject.SetActive(value: true);
-        }
-        GetComponent<CapsuleCollider2D>().enabled = true;
+        }*/
+
+        EnablePhysics();
         GetComponent<InputComponent>().enabled = true;
         _rb.gravityScale = _gravIni;
         GameManager.Instance.Respawne();
         _isDeath = false;
+        _isRespawn = false;
         GameManager.Instance.ResetSouls();
         Debug.Log("Resp");
         
-
-
     }
 }
