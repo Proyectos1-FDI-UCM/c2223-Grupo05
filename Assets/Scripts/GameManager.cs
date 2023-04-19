69155f8a4df004d331fec8d6030cae879897a1a8
Marc0sPM <marcospm0306@gmail.com>
2023-04-19 10:18:19 +0200

    Update GameManager.cs

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -41,7 +41,7 @@ public class GameManager : MonoBehaviour
 
     private void Awake()
     {
-        if(_instance != null) _instance = this;
+        if(_instance == null) _instance = this;
         else Destroy(this); 
     }
 
