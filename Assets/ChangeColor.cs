f4b5ebe45d48c910e08cfaa5a3e625fd69d74dc5
Marc0sPM <marcospm0306@gmail.com>
2023-04-21 12:42:12 +0200

    Menu skins (NO TOCAR MENU INCIAL PORFA)

--- Assets/ChangeColor.cs
+++ Assets/ChangeColor.cs
@@ -1,16 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.Rendering.Universal;
 
 public class ChangeColor : MonoBehaviour
 {
     [SerializeField] private Light2D _light;
- 
+    
     // Start is called before the first frame update
-    public void ChangeColors(Color color)
+    private void Start() //Player inicia el juego con el color asignado
     {
-        Debug.Log("ME CAGO EN DIOS");
+        _light.color = SkinManager.Instance.Col;
+    }
+    public void ChangeColors(Color color) //metodo de muestra en img player
+    {
         _light.color = color; 
     }
 }
