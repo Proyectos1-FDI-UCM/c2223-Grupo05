58f916909088e348525abd299b3d9f98626c238f
Mikeey84 <Milope21@ucm.es>
2023-04-21 13:48:45 +0200

    El mandoooo

--- Assets/Scripts/Controller/ControllerComponent.cs
+++ Assets/Scripts/Controller/ControllerComponent.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class ControllerComponent : MonoBehaviour
 {
+    private Vector3 _myCursorPosition;
+    private float _horizontal;
+    private float _vertical;
+    [SerializeField] private float _sensitivity;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +17,21 @@ public class ControllerComponent : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        _horizontal = Input.GetAxis("Joystick Right X");
+        _vertical = Input.GetAxis("Joystick Right Y");
+        _myCursorPosition = Input.mousePosition;
         
+        _myCursorPosition.x += _horizontal * _sensitivity;
+        _myCursorPosition.y += _vertical * _sensitivity;
+
+        _myCursorPosition.x = Mathf.Clamp(_myCursorPosition.x, 0, Screen.width);
+        _myCursorPosition.y = Mathf.Clamp(_myCursorPosition.y, 0, Screen.height);
+            
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Confined; //hace que el cursor no pueda salir de la ventana de juego
+        transform.position = _myCursorPosition;
+        
+        
+
     }
 }
