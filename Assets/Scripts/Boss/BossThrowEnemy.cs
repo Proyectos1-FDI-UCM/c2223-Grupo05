d531302da15be15aee103405c373e55819a9493c
Marc0sPM <marcospm0306@gmail.com>
2023-05-03 09:51:54 +0200

    Nuevo mecanica de lanzamiento de enemigos del boss

--- Assets/Scripts/Boss/BossThrowEnemy.cs
+++ Assets/Scripts/Boss/BossThrowEnemy.cs
@@ -1,14 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class BossThrowEnemy : MonoBehaviour
 {
     [SerializeField] private GameObject _myEnemyPrefab;
-    [SerializeField] Transform _enemySpawnPoint;
+    private Transform _enemySpawnPoint;
+    [SerializeField] private Transform[] _spawnList; 
     // Start is called before the first frame update
     public void SpawnEnemy()
     {
+        _enemySpawnPoint = _spawnList[Random.Range(0, _spawnList.Length)];
         SoundComponent.Instance.PlaySound(SoundComponent.Instance._bossEneymThrow);
         GameObject go = Instantiate(_myEnemyPrefab, _enemySpawnPoint.position, Quaternion.identity);
         go.GetComponentInChildren<ThownEnemyComponent>().SetSpin(this.transform.localScale.x);
